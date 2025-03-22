ac6d9143b4b7f9037b2b2f3179b59cb60f557daf
Infi <infiziert@protonmail.ch>
2025-03-22 15:11:52 +0100

    - Initial Release

---
+++ RepairMe/RecipeGeneration.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Vintagestory.API.Common;
+using Vintagestory.ServerMods;
+
+namespace RepairMe;
+
+public class RecipeGeneration : ModSystem
+{
+    public override bool ShouldLoad(EnumAppSide forSide) => forSide.IsServer();
+    public override double ExecuteOrder() => 1.02;
+    
+    public override void AssetsLoaded(ICoreAPI api)
+    {
+        var gridRecipeLoader = api.ModLoader.GetModSystem<GridRecipeLoader>();
+
+        var whetstone = api.World.GetItem(new AssetLocation("repairme", "whetstone"));
+        
+        foreach (var item in api.World.Items)
+        {
+            if (item.Code is null)
+                continue;
+
+            // Air has durability 1 for whatever reason
+            if (item.Durability < 2 || item.ItemClass != EnumItemClass.Item)
+                continue;
+            
+            // Prevent repair of the whetstone with a whetstone
+            if (item.ItemId == whetstone.ItemId)
+                continue;
+            
+            var recipe = new GridRecipe();
+            recipe.IngredientPattern = "W,T";
+            recipe.Width = 1;
+            recipe.Height = 2;
+            recipe.Shapeless = true;
+            recipe.AverageDurability = false;
+            recipe.Name = new AssetLocation("repairme", $"{item.Code.Path} repair");
+            recipe.CopyAttributesFrom = "T";
+            recipe.RecipeGroup = 1;
+            recipe.Output = new CraftingRecipeIngredient { Type = EnumItemClass.Item, Code = item.Code, };
+
+            recipe.Ingredients = new Dictionary<string, CraftingRecipeIngredient>()
+            {
+                { "W", new CraftingRecipeIngredient { Type = EnumItemClass.Item, Code = whetstone.Code, IsTool = true } },
+                { "T", new CraftingRecipeIngredient { Type = EnumItemClass.Item, Code = item.Code } }
+            };
+
+            gridRecipeLoader.LoadRecipe(new AssetLocation($"repairme:{item.Code.Path} repair"), recipe);
+        }
+    }
+}
---
+++ RepairMe/RepairItem.cs
@@ -0,0 +1,35 @@
+﻿using Vintagestory.API.Common;
+
+namespace RepairMe;
+
+public class RepairItem : Item
+{
+    public override void OnConsumedByCrafting(ItemSlot[] allInputSlots, ItemSlot stackInSlot, GridRecipe gridRecipe, CraftingRecipeIngredient fromIngredient, IPlayer byPlayer, int quantity)
+    {
+        if (!gridRecipe.Name.Path.Contains("repair"))
+        {
+            base.OnConsumedByCrafting(allInputSlots, stackInSlot, gridRecipe, fromIngredient, byPlayer, quantity);
+            return;
+        }
+        
+        var mouseSlotItem = byPlayer.InventoryManager.MouseItemSlot;
+        var changes = CalculateDurabilityChange(mouseSlotItem.Itemstack);
+        
+        stackInSlot.Itemstack.Collectible.DamageItem(byPlayer.Entity.World, byPlayer.Entity, stackInSlot, changes.WhetstoneDur);
+        mouseSlotItem.Itemstack.Item.SetDurability(mouseSlotItem.Itemstack, changes.ItemDur);
+    }
+
+    private (int WhetstoneDur, int ItemDur) CalculateDurabilityChange(ItemStack repairedTool)
+    {
+        var maxDurability = repairedTool.Item.GetMaxDurability(repairedTool);
+        var remainingDurability = repairedTool.Item.GetRemainingDurability(repairedTool);
+
+        var damagedDurability = (maxDurability / 100) * 5;
+        var repairedDurability = remainingDurability + ((maxDurability / 100) * 25);
+        
+        if (repairedDurability > maxDurability)
+            repairedDurability = maxDurability;
+        
+        return (damagedDurability, repairedDurability);
+    }
+}
